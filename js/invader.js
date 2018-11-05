f8858ff38a1aba5f24ee43cac4f5a9cafa76301b
s18003 <s18003@std.it-college.ac.jp>
2018-11-05 10:38:47 +0900

    アセットの定義とプレイヤースプライトの表示

--- js/invader.js
+++ js/invader.js
@@ -3,6 +3,36 @@ phina.globalize();
 
 const SCREEN_WIDTH = 960;
 const SCREEN_HEIGHT = 640;
+const ASSETS = {
+    "image": {
+        "buro": "../assets/image/buropiyo.png",
+        "mero": "../assets/image/meropiyo.png",
+        "mika": "../assets/image/mikapiyo.png",
+        "nasu": "../assets/image/nasupiyo.png",
+        "take": "../assets/image/takepiyo.png",
+        "toma": "../assets/image/tomapiyo.png"
+    }
+};
+
+phina.define('MainScene', {
+    superClass: 'DisplayScene',
+    init: function () {
+        this.superInit({
+            width: SCREEN_WIDTH,
+            height: SCREEN_HEIGHT,
+        });
+        this.gridX = Grid(SCREEN_WIDTH, 40);
+        this.gridY = Grid(SCREEN_HEIGHT, 40);
+
+        this.backgroundColor = 'black';
+
+        const player = Sprite('toma', 64, 64).addChildTo(this);
+        player.setFrameIndex(10, 64, 64);
+        player.x = this.gridX.center();
+        player.y = this.gridY.span(37);
+        
+    }
+});
 
 phina.main(() => {
     const app = GameApp({
@@ -10,6 +40,7 @@ phina.main(() => {
         fps: 60,
         width: SCREEN_WIDTH,
         height: SCREEN_HEIGHT,
+        assets: ASSETS,
     });
 
     app.run();
